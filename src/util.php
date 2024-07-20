7fecc23e830406a060b6fcfe24e4443767b62af4
ishidad2 <ishida_d@opening-line.co.jp>
2024-07-20 16:58:17 +0900

    add src folder

---
+++ src/util.php
@@ -0,0 +1,27 @@
+<?php
+use SymbolRestClient\Api\NodeRoutesApi;
+require_once(__DIR__ . '/vendor/autoload.php');
+
+use SymbolRestClient\Api\NetworkRoutesApi;
+use SymbolRestClient\Configuration;
+use SymbolSdk\Facade\SymbolFacade;
+
+$NODE_URL = 'http://sym-test-03.opening-line.jp:3000'; // 以前のコードスニペットからのURL
+
+$config = new Configuration();
+$config->setHost($NODE_URL);
+$client = new \GuzzleHttp\Client();
+
+// /node/info
+$nodeInfoApiInstance = new NodeRoutesApi($client, $config);
+$nodeInfo = $nodeInfoApiInstance->getNodeInfo();
+// /network/properties
+$networkType = $nodeInfo->getNetworkIdentifier();
+$generationHash = $nodeInfo->getNetworkGenerationHashSeed();
+
+$networkApiInstance = new NetworkRoutesApi($client, $config);
+$networkProperties = $networkApiInstance->getNetworkProperties();
+
+$epochAdjustment = $networkProperties->getNetwork()->getEpochAdjustment();
+$identifier = $networkProperties->getNetwork()->getIdentifier();
+$facade = new SymbolFacade('testnet');
