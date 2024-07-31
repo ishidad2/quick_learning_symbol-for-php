ada9955296fe11ee55eba5c55101a82a4e6e1d76
ishidad2 <ishida_d@opening-line.co.jp>
2024-07-31 10:16:27 +0900

    Update 13_verify

--- src/13_verify.php
+++ src/13_verify.php
@@ -1,8 +1,8 @@
 <?php
+require_once(__DIR__ . '/util.php');
 use SymbolRestClient\Api\AccountRoutesApi;
 use SymbolSdk\Merkle\MerkleHashBuilder;
 use SymbolSdk\Symbol\Models\Importance;
-require_once(__DIR__ . '/util.php');
 use SymbolRestClient\Api\BlockRoutesApi;
 use SymbolSdk\CryptoTypes\Signature;
 use SymbolSdk\Symbol\Models\Hash256;
@@ -18,7 +18,7 @@ $height = 686312;
 
 $tx = TransactionFactory::deserialize(hex2bin($payload));
 $hash = $facade->hashTransaction($tx);
-echo "===payload確認===" . PHP_EOL;
+// echo "===payload確認===" . PHP_EOL;
 // print_r($tx);
 
 /**
@@ -27,8 +27,8 @@ echo "===payload確認===" . PHP_EOL;
 
 $signature = new Signature($tx->signature);
 $res = $facade->verifyTransaction($tx, $signature);
-echo "===署名の検証===" . PHP_EOL;
-var_dump($res);
+// echo "===署名の検証===" . PHP_EOL;
+// var_dump($res);
 
 /**
  * マークルコンポーネントハッシュの計算
@@ -44,8 +44,8 @@ if (isset($tx->cosignatures) && count($tx->cosignatures) > 0) {
   }
   $merkleComponentHash = $hasher->final();
 }
-echo "===マークルコンポーネントハッシュ===" . PHP_EOL;
-echo strtoupper($merkleComponentHash) . PHP_EOL;
+// echo "===マークルコンポーネントハッシュ===" . PHP_EOL;
+// echo strtoupper($merkleComponentHash) . PHP_EOL;
 
 /**
  * InBlockの検証
@@ -66,31 +66,13 @@ $merkleProof = $blockApiInstance->getMerkleTransaction($height, $leafhash);
 $merklePath = $merkleProof["merkle_path"];
 
 $resutl = Merkle::proveMerkle($leafhash, $merklePath, $HRootHash);
-echo "===InBlockの検証===" . PHP_EOL;
-var_dump($resutl);
+// echo "===InBlockの検証===" . PHP_EOL;
+// var_dump($resutl);
 
 /**
  * ブロックヘッダーの検証
  */
-//normalブロックの検証
-/**
- * Converts a given number to a reversed hex string of specified byte length.
- *
- * @param int|string $number The number to convert.
- * @param int $bytes The byte length of the resulting hex string.
- * @return string The reversed hex string.
- */
-function reverseHex($number, $bytes = 1) {
-  // 10進数を16進数に変換し、必要に応じてゼロパディング
-  $hex = str_pad(dechex($number), $bytes * 2, "0", STR_PAD_LEFT);
-  // 16進数の文字列をバイナリデータに変換
-  $bin = hex2bin($hex);
-  // バイナリデータを逆順にする
-  $reversed = strrev($bin);
-  // バイナリデータを16進数の文字列に変換
-  $reversedHex = bin2hex($reversed);
-  return $reversedHex;
-}
+ //normalブロックの検証
 
 $blockInfo = $blockApiInstance->getBlockByHeight($height);
 $block = $blockInfo["block"];
@@ -122,8 +104,8 @@ if ($block['type'] === BlockType::NORMAL) {
 
   $hash = strtoupper(bin2hex(hash_final($hasher, true)));
 
-  echo "===ブロックヘッダーの検証===" . PHP_EOL;
-  var_dump($hash === $blockInfo['meta']['hash']);
+  // echo "===ブロックヘッダーの検証===" . PHP_EOL;
+  // var_dump($hash === $blockInfo['meta']['hash']);
 }
 
 /**
@@ -165,8 +147,8 @@ if($block['type'] === BlockType::IMPORTANCE){
 
   $hash = strtoupper(bin2hex(hash_final($hasher, true)));
 
-  echo "===importanceブロックの検証===" . PHP_EOL;
-  var_dump($hash === $blockInfo['meta']['hash']);
+  // echo "===importanceブロックの検証===" . PHP_EOL;
+  // var_dump($hash === $blockInfo['meta']['hash']);
 }
 
 /**
@@ -185,5 +167,214 @@ hash_update($hasher, hex2bin($blockInfo['meta']['state_hash_sub_cache_merkle_roo
 hash_update($hasher, hex2bin($blockInfo['meta']['state_hash_sub_cache_merkle_roots'][7]));
 hash_update($hasher, hex2bin($blockInfo['meta']['state_hash_sub_cache_merkle_roots'][8]));
 $hash = strtoupper(bin2hex(hash_final($hasher, true)));
-echo "===stateHashの検証===" . PHP_EOL;
-var_dump($hash === $blockInfo['block']['state_hash']);
+// echo "===stateHashの検証===" . PHP_EOL;
+// var_dump($hash === $blockInfo['block']['state_hash']);
+
+/**
+ * 13.3 アカウント・メタデータの検証
+ */
+
+ //検証用共通関数
+
+function reverseHex($number, $bytes = 1) {
+  // 10進数を16進数に変換し、必要に応じてゼロパディング
+  $hex = str_pad(dechex($number), $bytes * 2, "0", STR_PAD_LEFT);
+  // 16進数の文字列をバイナリデータに変換
+  $bin = hex2bin($hex);
+  // バイナリデータを逆順にする
+  $reversed = strrev($bin);
+  // バイナリデータを16進数の文字列に変換
+  $reversedHex = bin2hex($reversed);
+  return $reversedHex;
+}
+
+//葉のハッシュ値取得関数
+function getLeafHash($encodedPath, $leafValue) {
+  $hasher = hash_init('sha3-256');
+  hash_update($hasher, hex2bin($encodedPath . $leafValue));
+  $hash = strtoupper(bin2hex(hash_final($hasher, true)));
+  return $hash;
+}
+// getLeafHash("200F84DD2830B37539EF766DD37A0DA6150FB8E14AEE2ED2773262F4AF14CF","39B9DF440E50AF995D7E8DD94FA38BF68033CC39053B8C9FA1BFC2AA25C99F91");
+
+// 枝のハッシュ値取得関数
+function getBranchHash($encodedPath, $links) {
+  $branchLinks = array_fill(0, 16, bin2hex(str_repeat(chr(0), 32)));
+  foreach ($links as $link) {
+      $index = hexdec($link['bit']);
+      $branchLinks[$index] = $link['link'];
+  }
+  $concatenated = $encodedPath . implode("", $branchLinks);
+  $hasher = hash_init('sha3-256');
+  hash_update($hasher, hex2bin($concatenated));
+
+  return strtoupper(bin2hex(hash_final($hasher, true)));
+}
+// $array = [
+//   ["bit" => 2, "link" => "513DB50C2C5D5ADFEE727C4DAB2FD15D67D445DACE0EE4A91D2316BB6B5184DB"],
+//   ["bit" => 4, "link" => "B6BFC203326047B79F350E8D397B7278B52B05B8061CBAE31A1E3E75EB374A11"],
+//   ["bit" => 5, "link" => "A196EDBEDD6025B5A1C91D07033067FD7C231954FFD68CA7845053F6E0DECDE9"],
+//   ["bit" => 6, "link" => "BE463563FE9855B88AC5EB5D4C648FCD0ACBF88FB470B4A156C6DF699469A7B0"],
+//   ["bit" => 7, "link" => "3879965782C68D7BAFD9A3A5318606AC7720633E05F16A74998C979CE7CF6F4D"],
+//   ["bit" => 8, "link" => "D92E9488B9A54EBF96760969B05E260B97E4FD0C4BCA87193ED88CC50C6A6610"],
+//   ["bit" => 9, "link" => "D5AD2127DD636D531F26609A6F0055BB5B607C978BF4E8C7E3515125A76A956B"],
+//   ["bit" => "A", "link" => "65DFF47A75A11595830C0B6E03E44DC3E869A57EFC51632350B4A999D69BB24E"],
+//   ["bit" => "B", "link" => "38E66E2F0B029AE498F6EE0DC22C4F2836AF9F0E02B7E4EDD94A13EF0451FDA5"],
+//   ["bit" => "C", "link" => "17F2795C8028161B541527BE46B12353CCADE3E01FCEF52C71FE5BC6AEDEA6B7"],
+//   ["bit" => "E", "link" => "E64C24870C17F255F96E19120929AF366C81ADCDFB585D9976F3F26C855EF71C"],
+//   ["bit" => "F", "link" => "AAC621E07225A53114FBDE5A4EA0C79860743C0122F25A26BAAD0638C697E5FD"]
+// ];
+// $res = getBranchHash('00', $array);
+// echo $res . PHP_EOL;
+
+// ワールドステートの検証
+
+function checkState($stateProof, $stateHash, $pathHash, $rootHash) {
+  $merkleLeaf = null;
+  $merkleBranches = [];
+  foreach($stateProof['tree'] as $n){
+    if($n['type'] === 255){
+      $merkleLeaf = $n;
+    } else {
+      $merkleBranches[] = $n;
+    }
+  }
+  $merkleBranches = array_reverse($merkleBranches);
+  $leafHash = getLeafHash($merkleLeaf['encoded_path'], $stateHash);
+
+  $linkHash = $leafHash;  // リンクハッシュの初期値は葉ハッシュ
+  $bit = "";
+  for($i=0; $i <  count($merkleBranches); $i++){
+    $branch = $merkleBranches[$i];
+    $branchLink = array_filter($branch['links'], function($link) use ($linkHash) {
+      return $link['link'] === $linkHash;
+    });
+    $branchLink = reset($branchLink); // 最初の要素を取得
+    $linkHash = getBranchHash($branch['encoded_path'], $branch['links']);
+    $bit = substr($merkleBranches[$i]['path'], 0, $merkleBranches[$i]['nibble_count']) . $branchLink['bit'] . $bit;
+  }
+  $treeRootHash = $linkHash; //最後のlinkHashはrootHash
+  $treePathHash = $bit . $merkleLeaf['path'];
+  if(strlen($treePathHash) % 2 == 1){
+    $treePathHash = substr($treePathHash, 0, -1);
+  }
+
+  // 検証
+  var_dump($treeRootHash === $rootHash);
+  var_dump($treePathHash === $pathHash);
+
+}
+
+function hexToUint8($hex) {
+  // 16進数文字列をバイナリデータに変換
+  $binary = hex2bin($hex);
+  // バイナリデータを配列に変換
+  return array_values(unpack('C*', $binary));
+}
+
+$aliceRawAddress = "TBIL6D6RURP45YQRWV6Q7YVWIIPLQGLZQFHWFEQ";
+$aliceAddress = new Address($aliceRawAddress);
+
+$hasher = hash_init('sha3-256');
+$alicePathHash = hash_update($hasher, hex2bin($aliceAddress));
+$alicePathHash = strtoupper(bin2hex(hash_final($hasher, true)));
+
+$hasher = hash_init('sha3-256');
+$accountApiInstance = new AccountRoutesApi($client, $config);
+$aliceInfo = $accountApiInstance->getAccountInfo($aliceRawAddress);
+$aliceInfo = $aliceInfo["account"];
+
+// アカウント情報から StateHash を導出
+$format = (int)$aliceInfo['importance'] === 0 || strlen($aliceInfo['activity_buckets']) <5 ? 0x00 : 0x01;
+
+$supplementalPublicKeysMask = 0x00;
+$linkedPublicKey = [];
+if($aliceInfo['supplemental_public_keys']['linked'] !== null){
+  $supplementalPublicKeysMask |= 0x01;  // OR 演算子と代入演算子を組み合わせ
+  $linkedPublicKey = hexToUint8($aliceInfo['supplemental_public_keys']['linked']['public_key']);
+}
+
+$nodePublicKey = [];
+if($aliceInfo['supplemental_public_keys']['node'] !== null){
+  $supplementalPublicKeysMask |= 0x02;
+  $nodePublicKey = hexToUint8($aliceInfo['supplemental_public_keys']['node']['public_key']);
+}
+
+$vrfPublicKey = [];
+if($aliceInfo['supplemental_public_keys']['vrf'] !== null){
+  $supplementalPublicKeysMask |= 0x04;
+  $vrfPublicKey = hexToUint8($aliceInfo['supplemental_public_keys']['vrf']['public_key']);
+}
+
+$votingPublicKeys = [];
+if($aliceInfo['supplemental_public_keys']['voting'] !== null){
+  foreach($aliceInfo['supplemental_public_keys']['voting']['public_key'] as $key){
+    $votingPublicKeys = array_merge($votingPublicKeys, hexToUint8($key['public_key']));
+  }
+}
+
+$importanceSnapshots = [];
+if((int)$aliceInfo['importance'] !== 0){
+  $importanceSnapshots = array_merge(
+    hexToUint8(reverseHex($aliceInfo['importance_snapshot'], 8)),
+    hexToUint8(reverseHex($aliceInfo['importance_snapshot_height'], 8))
+  );
+}
+
+$activityBuckets = [];
+if((int)$aliceInfo['importance'] > 0){
+  for ($idx = 0; $idx < count($aliceInfo['activity_buckets']) || $idx < 5; $idx++) {
+    $bucket = $aliceInfo['activity_buckets'][$idx];
+    $activityBuckets = array_merge(
+      $activityBuckets,
+      hexToUint8(reverseHex($bucket['start_height'], 8)),
+      hexToUint8(reverseHex($bucket['total_fees_paid'], 8)),
+      hexToUint8(reverseHex($bucket['beneficiary_count'], 4)),
+      hexToUint8(reverseHex($bucket['raw_score'], 8))
+    );
+  }
+}
+
+$balances = [];
+if(count($aliceInfo['mosaics']) > 0){
+  foreach($aliceInfo['mosaics'] as $mosaic){
+    $balances = array_merge(
+      $balances,
+      hexToUint8(bin2hex(strrev(hex2bin($mosaic['id'])))),
+      hexToUint8(reverseHex($mosaic['amount'], 8))
+    );
+  }
+}
+
+$accountInfoBytes = array_merge(
+  hexToUint8(reverseHex($aliceInfo['version'], 2)),
+  hexToUint8($aliceInfo['address']),
+  hexToUint8(reverseHex($aliceInfo['address_height'], 8)),
+  hexToUint8($aliceInfo['public_key']),
+  hexToUint8(reverseHex($aliceInfo['public_key_height'], 8)),
+  hexToUint8(reverseHex($aliceInfo['account_type'], 1)),
+  hexToUint8(reverseHex($format, 1)),
+  hexToUint8(reverseHex($supplementalPublicKeysMask, 1)),
+  hexToUint8(reverseHex(count($votingPublicKeys), 1)),
+  $linkedPublicKey,
+  $nodePublicKey,
+  $vrfPublicKey,
+  $votingPublicKeys,
+  $importanceSnapshots,
+  $activityBuckets,
+  hexToUint8(reverseHex(count($aliceInfo['mosaics']), 2)),
+  $balances,
+);
+// var_dump($accountInfoBytes);
+
+$accountInfoBytesString = implode('', array_map('chr', $accountInfoBytes));
+
+hash_update($hasher, $accountInfoBytesString);
+$aliceStateHash = strtoupper(bin2hex(hash_final($hasher, true)));
+
+var_dump($aliceStateHash);
+
+require_once(__DIR__ . '/13_sample_data.php');
+checkState($stateProofSample, $stateHashSample, $pathHashSample, $rootHashSample);
+
+
